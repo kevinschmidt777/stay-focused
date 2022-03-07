241c1ba43cb3e8c72b6ec182826465db4f3678bc
Kevin Schmidt <kevin@kswebentwicklung.de>
2022-03-07 17:11:14 +0100

    1

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,4 +1,11 @@
+import style from "./index.module.scss";
+
 const Index = () => {
-  return <div>yay</div>;
+  return (
+    <div className={style.index}>
+      <div className="text-handwritten">Stay focused</div>
+      <div className={style.button}>Start</div>
+    </div>
+  );
 };
 export default Index;
