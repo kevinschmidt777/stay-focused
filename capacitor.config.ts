bc4624dd74f153a936e9afd469a09d99ba65f521
Kevin Schmidt <kevinschmidt777@hotmail.de>
2022-03-15 15:40:57 +0100

    Basic capacitor with iOS

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "focus.kswebentwicklung.de",
+  appName: "stay-focused",
+  webDir: "./",
+  bundledWebRuntime: false,
+};
+
+export default config;
